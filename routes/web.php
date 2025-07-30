78a1c1a55b6cb8d1f15476119035f0b5babd610f
asad <ashad731386@gmail.com>
2025-07-31 01:53:35 +0600

    book and student only table controller and folder stracture done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function(){
     return view('pages.deshboard');
 });
+
+
+Route::resource("/student" , StudentController::class);
+Route::resource("/books" , BookController::class);
